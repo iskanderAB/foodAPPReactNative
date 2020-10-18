cd7991eae84b7b617001542f141f1778315bfedc
iskanderAB <57728067+iskanderAB@users.noreply.github.com>
2020-10-18 02:40:56 +0100

    Update yelp.js
    Security

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: ' https://api.yelp.com/v3/businesses/',
-    headers: {
-        Authorization : 'Bearer ubPq_mjvr55Db5AVc8j1TPoo2wIFO9Gxf8u3V_vMLmi2UbrOi1q0GZ-cQbihYY_a-wnjjwHQDRFYyo4c6T0Cwd1qCxVaa-v2wnBv6CK1p1sxdBr7xoq7GdmU4yGCX3Yx'
+    baseURL: ' https://api.yelp.com/v3/businesses/'
     }
 });
